62dae1b9eea24ccbd0703221e3c84345fb0cb4d8
Meghan31 <pulakhandammegha@gmail.com>
2022-07-07 13:26:27 +0530

    🎉 Foodie Fav App

---
+++ foodie_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FoodieFav',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyText2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            subtitle1: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
