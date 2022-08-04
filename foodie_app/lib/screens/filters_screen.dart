68b163e6a14457e05375db8e4a0acc2be2944c41
Meghan31 <pulakhandammegha@gmail.com>
2022-08-04 21:09:14 +0530

    ✨ Added switches for filters screen✨

--- foodie_app/lib/screens/filters_screen.dart
+++ foodie_app/lib/screens/filters_screen.dart
@@ -1,12 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_complete_guide/widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
-  static const ro
+class FilterScreen extends StatefulWidget {
+  static const routeName = '/filters';
   const FilterScreen({Key key}) : super(key: key);
 
   @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTiles(
+    String title,
+    String description,
+    var currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Your Filters'),
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Your meal selection',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                // 1
+                _buildSwitchListTiles(
+                  'Gluten-free',
+                  'includes Gluteen free meals',
+                  _glutenFree,
+                  (value) {
+                    setState(
+                      () {
+                        _glutenFree = value;
+                      },
+                    );
+                  },
+                ),
+                // 2
+                _buildSwitchListTiles(
+                  'Lactose-free',
+                  'includes Lactose free meals',
+                  _lactoseFree,
+                  (value) {
+                    setState(
+                      () {
+                        _lactoseFree = value;
+                      },
+                    );
+                  },
+                ),
+                // 3
+                _buildSwitchListTiles(
+                  'Vegetarian',
+                  'includes Vegetarian meals',
+                  _vegetarian,
+                  (value) {
+                    setState(
+                      () {
+                        _vegetarian = value;
+                      },
+                    );
+                  },
+                ),
+                // 4
+                _buildSwitchListTiles(
+                  'Vegan',
+                  'includes Vegan meals',
+                  _vegan,
+                  (value) {
+                    setState(
+                      () {
+                        _vegan = value;
+                      },
+                    );
+                  },
+                ),
+              ],
+            ))
+          ],
+        ));
   }
 }
